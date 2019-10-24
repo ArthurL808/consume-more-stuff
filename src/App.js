350a570951505ea9ecf65c742bcb608567f9066c
frzrbox <david@freezerbox.dev>
2019-10-23 21:38:04 -1000

    adds sidebar and main body comopnents

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.scss';
 
 import Header from './Components/Header'
+import Sidebar from './Components/Sidebar'
+import MainBody from './Components/MainBody'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="main">
+        <Sidebar />
+        <MainBody />
+      </div>
+
     </div>
   );
 }
---
+++ src/Components/MainBody/MainBody.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MainBody.module.scss'
+
+const MainBody = () => {
+    return (
+        <div className={styles.main_body}>
+            this is the body
+        </div>
+    )
+}
+
+export default MainBody;
