d6c3b245916b25c1dcd4fccacffd930baee97b89
Briii <brianwkhuffman@gmail.com>
2019-10-23 21:44:51 -1000

    Making category table migration

--- database/migrations/20191023205531_create-items.js
+++ database/migrations/20191023205531_create-items.js
@@ -3,8 +3,9 @@ exports.up = function (knex) {
     return knex.schema.createTable('items', (table) => {
         table.increments();
         table.string('name').notNullable();
-        table.string('description');
+        table.string('description').notNullable();
         table.integer('price').notNullable();
+        table.string('manufacturer');
         table.integer('user_id').notNullable().references('id').inTable('users');
         table.integer('category_id').notNullable().references('id').inTable('categories');
         table.integer('itemStatus_id').notNullable().references('id').inTable('itemStatuses');
