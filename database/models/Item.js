f25df91a31216656e7c93ff8348b9129584c8848
Briii <brianwkhuffman@gmail.com>
2019-10-24 19:13:23 -1000

    Making Item model

---
+++ database/models/Item.js
@@ -0,0 +1,24 @@
+const bookshelf = require('../bookshelf');
+
+class Item extends bookshelf.Model {
+    get tableName() {
+        return 'item';
+    }
+    get hasTimestamps() {
+        return true;
+    }
+    user() {
+        return this.belongsTo('User');
+    }
+    category() {
+        return this.belongsTo('Category');
+    }
+    itemStatus() {
+        return this.belongsTo('ItemStatuses');
+    }
+    condition() {
+        return this.belongsTo('Conditions');
+    }
+};
+
+module.exports = bookshelf.Model('Item', Item);
