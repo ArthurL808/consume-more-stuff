18082224ba057a23b8d2e5a4f1f1bbf466cc73c9
Arthur <kingart808@gmail.com>
2019-10-24 21:02:40 -1000

    changed price values to be integer instead of strings

--- database/seeds/items.js
+++ database/seeds/items.js
@@ -5,185 +5,185 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('items').insert([{
-        "name": "Gwyn",
-        "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
-        "manufacturer": "Chevrolet",
-        "price": "$0.25",
+        "name": "Sabra",
+        "description": "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.\n\nPellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+        "manufacturer": "Dodge",
+        "price": 71.96,
         "user_id": 5,
-        "category_id": 2,
-        "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "Lorianna",
-        "description": "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
-        "manufacturer": "Maybach",
-        "price": "$6.39",
-        "user_id": 1,
         "category_id": 3,
         "itemStatus_id": 2,
         "condition_id": 2
       }, {
-        "name": "Frannie",
-        "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
-        "manufacturer": "Hyundai",
-        "price": "$2.59",
-        "user_id": 2,
-        "category_id": 2,
-        "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "Mord",
-        "description": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
-        "manufacturer": "Honda",
-        "price": "$3.04",
-        "user_id": 5,
-        "category_id": 1,
-        "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "Ania",
-        "description": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-        "manufacturer": "Lincoln",
-        "price": "$2.30",
-        "user_id": 3,
-        "category_id": 2,
-        "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "Engelbert",
-        "description": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
-        "manufacturer": "Jeep",
-        "price": "$5.87",
-        "user_id": 3,
-        "category_id": 1,
-        "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "West",
-        "description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
-        "manufacturer": "Toyota",
-        "price": "$5.70",
-        "user_id": 2,
-        "category_id": 3,
-        "itemStatus_id": 2,
-        "condition_id": 1
-      }, {
-        "name": "Ahmed",
-        "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.",
+        "name": "Brandy",
+        "description": "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
         "manufacturer": "Ford",
-        "price": "$1.50",
-        "user_id": 3,
-        "category_id": 3,
-        "itemStatus_id": 2,
-        "condition_id": 1
-      }, {
-        "name": "Lianna",
-        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.",
-        "manufacturer": "Oldsmobile",
-        "price": "$5.70",
+        "price": 14.14,
         "user_id": 5,
-        "category_id": 3,
-        "itemStatus_id": 1,
-        "condition_id": 2
-      }, {
-        "name": "Michel",
-        "description": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
-        "manufacturer": "Toyota",
-        "price": "$6.86",
-        "user_id": 3,
-        "category_id": 3,
+        "category_id": 1,
         "itemStatus_id": 2,
         "condition_id": 1
       }, {
-        "name": "Madonna",
-        "description": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.",
-        "manufacturer": "Cadillac",
-        "price": "$9.00",
+        "name": "Putnem",
+        "description": "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
+        "manufacturer": "Ford",
+        "price": 63.95,
         "user_id": 2,
         "category_id": 1,
         "itemStatus_id": 2,
         "condition_id": 1
       }, {
-        "name": "Lucio",
-        "description": "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.\n\nSuspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
-        "manufacturer": "Chrysler",
-        "price": "$0.40",
+        "name": "Amil",
+        "description": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
+        "manufacturer": "Toyota",
+        "price": 25.71,
+        "user_id": 3,
+        "category_id": 1,
+        "itemStatus_id": 2,
+        "condition_id": 2
+      }, {
+        "name": "Fairlie",
+        "description": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+        "manufacturer": "Ford",
+        "price": 85.22,
         "user_id": 5,
-        "category_id": 3,
+        "category_id": 1,
         "itemStatus_id": 1,
         "condition_id": 1
       }, {
-        "name": "Chastity",
-        "description": "Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
-        "manufacturer": "Audi",
-        "price": "$1.84",
+        "name": "Hamlin",
+        "description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+        "manufacturer": "Pontiac",
+        "price": 51.66,
+        "user_id": 3,
+        "category_id": 3,
+        "itemStatus_id": 2,
+        "condition_id": 2
+      }, {
+        "name": "Faber",
+        "description": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.",
+        "manufacturer": "Toyota",
+        "price": 98.87,
+        "user_id": 3,
+        "category_id": 1,
+        "itemStatus_id": 1,
+        "condition_id": 1
+      }, {
+        "name": "Tatum",
+        "description": "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+        "manufacturer": "Mitsubishi",
+        "price": 27.53,
+        "user_id": 2,
+        "category_id": 3,
+        "itemStatus_id": 2,
+        "condition_id": 2
+      }, {
+        "name": "Gery",
+        "description": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+        "manufacturer": "Chevrolet",
+        "price": 14.98,
+        "user_id": 1,
+        "category_id": 2,
+        "itemStatus_id": 2,
+        "condition_id": 1
+      }, {
+        "name": "Alex",
+        "description": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+        "manufacturer": "Dodge",
+        "price": 84.67,
+        "user_id": 2,
+        "category_id": 2,
+        "itemStatus_id": 2,
+        "condition_id": 2
+      }, {
+        "name": "Holt",
+        "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+        "manufacturer": "Porsche",
+        "price": 82.81,
+        "user_id": 3,
+        "category_id": 3,
+        "itemStatus_id": 2,
+        "condition_id": 1
+      }, {
+        "name": "Ari",
+        "description": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+        "manufacturer": "MINI",
+        "price": 28.95,
+        "user_id": 1,
+        "category_id": 2,
+        "itemStatus_id": 2,
+        "condition_id": 2
+      }, {
+        "name": "Kermit",
+        "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+        "manufacturer": "Honda",
+        "price": 69.77,
+        "user_id": 3,
+        "category_id": 1,
+        "itemStatus_id": 2,
+        "condition_id": 2
+      }, {
+        "name": "Vaclav",
+        "description": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.",
+        "manufacturer": "Mazda",
+        "price": 64.22,
+        "user_id": 1,
+        "category_id": 1,
+        "itemStatus_id": 2,
+        "condition_id": 1
+      }, {
+        "name": "Alfonso",
+        "description": "Fusce consequat. Nulla nisl. Nunc nisl.\n\nDuis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.",
+        "manufacturer": "Nissan",
+        "price": 12.46,
+        "user_id": 3,
+        "category_id": 3,
+        "itemStatus_id": 2,
+        "condition_id": 2
+      }, {
+        "name": "Tobias",
+        "description": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+        "manufacturer": "Honda",
+        "price": 93.0,
+        "user_id": 3,
+        "category_id": 1,
+        "itemStatus_id": 1,
+        "condition_id": 2
+      }, {
+        "name": "Kellen",
+        "description": "In congue. Etiam justo. Etiam pretium iaculis justo.",
+        "manufacturer": "Suzuki",
+        "price": 75.2,
         "user_id": 4,
         "category_id": 1,
         "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "Marget",
-        "description": "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
-        "manufacturer": "Audi",
-        "price": "$7.02",
-        "user_id": 3,
-        "category_id": 2,
-        "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "Margret",
-        "description": "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
-        "manufacturer": "Chrysler",
-        "price": "$4.26",
-        "user_id": 5,
-        "category_id": 3,
-        "itemStatus_id": 2,
-        "condition_id": 1
-      }, {
-        "name": "Ricky",
-        "description": "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.\n\nVestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.",
-        "manufacturer": "Toyota",
-        "price": "$1.08",
-        "user_id": 2,
-        "category_id": 2,
-        "itemStatus_id": 2,
         "condition_id": 2
       }, {
-        "name": "Hamil",
-        "description": "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
-        "manufacturer": "Aston Martin",
-        "price": "$9.12",
-        "user_id": 5,
-        "category_id": 2,
-        "itemStatus_id": 1,
-        "condition_id": 2
-      }, {
-        "name": "Anitra",
-        "description": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
-        "manufacturer": "Austin",
-        "price": "$3.11",
-        "user_id": 5,
-        "category_id": 1,
-        "itemStatus_id": 1,
-        "condition_id": 1
-      }, {
-        "name": "Braden",
-        "description": "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
-        "manufacturer": "Mazda",
-        "price": "$5.02",
-        "user_id": 3,
-        "category_id": 2,
-        "itemStatus_id": 2,
-        "condition_id": 1
-      }, {
-        "name": "Skipper",
-        "description": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        "name": "Mortimer",
+        "description": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.",
         "manufacturer": "Dodge",
-        "price": "$9.39",
+        "price": 44.91,
+        "user_id": 3,
+        "category_id": 2,
+        "itemStatus_id": 1,
+        "condition_id": 2
+      }, {
+        "name": "Rozanne",
+        "description": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+        "manufacturer": "Plymouth",
+        "price": 86.33,
+        "user_id": 3,
+        "category_id": 1,
+        "itemStatus_id": 2,
+        "condition_id": 1
+      }, {
+        "name": "Gretna",
+        "description": "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
+        "manufacturer": "Aston Martin",
+        "price": 13.27,
         "user_id": 5,
         "category_id": 2,
         "itemStatus_id": 2,
-        "condition_id": 2
+        "condition_id": 1
       }]);
     });
 };
