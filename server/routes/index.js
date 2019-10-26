ba1b84f5c60b5ba5a506f9907ee21a6e93d9edcc
Arthur <kingart808@gmail.com>
2019-10-26 13:23:12 -1000

    imported items and users routes into index.js then exported as object into server file

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const itemsRoutes = require('./items/items')
+const userRoutes = require('./users/users')
+
+module.exports= {
+    items: itemsRoutes,
+    users: userRoutes
+}
