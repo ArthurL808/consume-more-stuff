793e9243f96409c1cfaf1177bea9e578d17f9d12
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-10-26 14:14:28 -1000

    fixing again

--- src/Components/MainBody/MainBody.jsx
+++ src/Components/MainBody/MainBody.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./MainBody.module.scss";
 // IMPORT ALL BODY COMPOENENTS HERE
-import Auth from "../Auth";
+
 import UnAuth from "../UnAuth";
 
 const MainBody = ({ isAuth }) => {
   return (
     <div className={styles.main_body}>
       {/*ALL CONTENT FOR THE BODY SECTION GOES HERE*/}
-      {isAuth ? <Auth /> : <UnAuth view={view} />}
+      {isAuth ? <h1>Logged In</h1> : <UnAuth view={view} />}
     </div>
   );
 };
