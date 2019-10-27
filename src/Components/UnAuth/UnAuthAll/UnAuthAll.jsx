06e38d40dee63d80a2ad5c40177095c1c66dde6b
frzrbox <david@freezerbox.dev>
2019-10-26 19:30:02 -1000

    fetch data works

--- src/Components/UnAuth/UnAuthAll/UnAuthAll.jsx
+++ src/Components/UnAuth/UnAuthAll/UnAuthAll.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import styles from './UnAuthAll.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+// import styles from './UnAuthAll.module.scss';
+import { connect } from 'react-redux';
 import { loadItemsAsync } from '../../../actions';
-import reducer from '../../../reducers';
 
-const UnAuthAll = () => {
+const UnAuthAll = (props) => {
 
-    const items = useSelector(state => state.items)
+    const { dispatch, items } = props;
+
+    useEffect(() => {
+        dispatch(loadItemsAsync())
+    }, [dispatch])
 
     return (
         <div>
-            {console.log(items)}
+            {items && (items.map(item => (
+                <h1>{item.name}</h1>
+            )))}
         </div>
     )
 }
 
-export default UnAuthAll;
+const mapStateToProps = (state) => {
+    return {
+        items: state.items
+    }
+}
+
+export default connect(mapStateToProps)(UnAuthAll);
