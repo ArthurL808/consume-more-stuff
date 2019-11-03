ecb9ed55a67679019a9cab01eafbbc6b75d65727
frzrbox <david@freezerbox.dev>
2019-11-02 14:37:01 -1000

    deletes unused components and routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import UnAuthAll from './Components/UnAuth/UnAuthAll';
-import UnAuthSoftware from './Components/UnAuth/UnAuthSoftware';
-import UnAuthAccessories from './Components/UnAuth/UnAuthAccessories';
-import UnAuthMisc from './Components/UnAuth/UnAuthMisc';
-import UnAuthHardware from './Components/UnAuth/UnAuthHardware';
 import AuthNewItem from './Components/Auth/AuthNewItem';
 import AuthHome from './Components/Auth/AuthHome';
 import AuthMessages from './Components/Auth/AuthMessages';
@@ -13,23 +8,18 @@ export const routes = [
     {
         path: '/all',
         exact: true,
-        // main: () => <UnAuthAll />
     },
     {
-        path: '/software',
-        // main: () => <UnAuthSoftware />
+        path: '/software'
     },
     {
-        path: '/hardware',
-        // main: () => <UnAuthHardware />
+        path: '/hardware'
     },
     {
-        path: '/accessories',
-        // main: () => <UnAuthAccessories />
+        path: '/accessories'
     },
     {
-        path: '/misc',
-        // main: () => <UnAuthMisc />
+        path: '/misc'
     },
     {
         path: '/home',
