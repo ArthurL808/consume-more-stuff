50c81a7b4356243902901d8d27508126a39cd4b9
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-11-02 17:30:24 -1000

    auth home view scroll buttons working

--- src/Components/Auth/AuthHome/AuthHomeFilter/AuthHomeFilter.jsx
+++ src/Components/Auth/AuthHome/AuthHomeFilter/AuthHomeFilter.jsx
@@ -1,27 +1,65 @@
 import React, { Component } from "react";
-import styles from "./AuthHomeFilter.scss";
+import styles from "./AuthHomeFilter.module.scss";
 import { Link } from "react-router-dom";
+import { Spring, animated } from "react-spring/renderprops";
 
 class AuthHomeFilter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({ value: this.state.value + 50 });
+  };
+
+  decrease = () => {
+    this.setState({ value: this.state.value - 50 });
+  };
+
   render() {
     return (
       <>
         <div className={styles.container}>
+          <button onClick={this.increment} className={styles.arrowLeft}>
+            {" "}
+            &larr;{" "}
+          </button>
+          <button onClick={this.decrease} className={styles.arrowRight}>
+            {" "}
+            &rarr;{" "}
+          </button>
+
           <h2>{this.props.filter}</h2>
-          {this.props.items &&
-            this.props.items.map(
-              item =>
-                item.itemStatus.status === this.props.filter && (
-                  <div key={item.id}>
-                    <Link to="/home">
-                      <img src="https://placekitten.com/200/150" alt="Meow" />
-                    </Link>
-                    <h3>{item.name}</h3>
-                    <p>{item.manufacturer}</p>
-                    <p>{item.price}</p>
-                  </div>
-                )
+
+          <Spring from={{ x: this.state.value }} to={{ x: this.state.value }}>
+            {props => (
+              <animated.div
+                className={styles.prodList}
+                style={{ transform: `translate3d(${props.x}px, 0, 0)` }}
+              >
+                {this.props.items &&
+                  this.props.items.map(
+                    item =>
+                      item.itemStatus.status === this.props.filter && (
+                        <div key={item.id}>
+                          <Link to="/home">
+                            <div className={styles.imageContainer}>
+                              <img src={item.imageUrl} alt="Meow" />
+                            </div>
+                          </Link>
+                          <h3>Name: {item.name}</h3>
+                          <p>Manufacturer: {item.manufacturer}</p>
+                          <p>Price: {item.price}</p>
+                        </div>
+                      )
+                  )}
+              </animated.div>
             )}
+          </Spring>
         </div>
       </>
     );
