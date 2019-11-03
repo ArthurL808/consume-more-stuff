e53bc7bf2d9ce2900761262ae1621f8cd72658a3
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-11-02 15:08:49 -1000

    fixed the filter to show with new item status

--- src/Components/Auth/AuthHome/AuthHomeFilter/AuthHomeFilter.jsx
+++ src/Components/Auth/AuthHome/AuthHomeFilter/AuthHomeFilter.jsx
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
-// import styles from "./AuthHomeFilter.scss";
+import styles from "./AuthHomeFilter.scss";
 import { Link } from "react-router-dom";
 
 class AuthHomeFilter extends Component {
-  getStatus(e) {
-    if (e === 1) {
-      return <h3>Pending</h3>;
-    } else if (e === 2) {
-      return <h3>Published</h3>;
-    } else if (e === 3) {
-      return <h3>Sold</h3>;
-    }
-  }
-
-  //link to item needs to be fixed
-
   render() {
     return (
       <>
-        {this.getStatus(this.props.filter)}
-        <div>
+        <div className={styles.container}>
+          <h2>{this.props.filter}</h2>
           {this.props.items &&
             this.props.items.map(
               item =>
-                item.itemStatus_id === this.props.filter && (
+                item.itemStatus.status === this.props.filter && (
                   <div key={item.id}>
                     <Link to="/home">
                       <img src="https://placekitten.com/200/150" alt="Meow" />
