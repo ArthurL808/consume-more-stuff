5ef3c610c2e682dea7844784f263c9c07d31eae9
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-11-05 00:51:23 -1000

    created new route for detailed item and made edit item component

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import AuthNewItem from './Components/Auth/AuthNewItem';
-import AuthHome from './Components/Auth/AuthHome';
-import AuthMessages from './Components/Auth/AuthMessages';
-import AuthSettings from './Components/Auth/AuthSettings';
+import React from "react";
+import AuthNewItem from "./Components/Auth/AuthNewItem";
+import AuthHome from "./Components/Auth/AuthHome";
+import AuthMessages from "./Components/Auth/AuthMessages";
+import AuthSettings from "./Components/Auth/AuthSettings";
+import AuthEditItem from "./Components/Auth/AuthEditItem";
 
 export const routes = [
-    {
-        path: '/home',
-        main: () => <AuthHome />
-    },
-    {
-        path: '/new',
-        main: () => <AuthNewItem />
-    },
-    {
-        path: '/messages',
-        main: () => <AuthMessages />
-    },
-    {
-        path: '/settings',
-        main: () => <AuthSettings />
-    }
-]
+  {
+    path: "/home",
+    main: () => <AuthHome />
+  },
+  {
+    path: "/new",
+    main: () => <AuthNewItem />
+  },
+  {
+    path: "/messages",
+    main: () => <AuthMessages />
+  },
+  {
+    path: "/settings",
+    main: () => <AuthSettings />
+  },
+  {
+    path: `/edit/:id`,
+    main: () => <AuthEditItem />
+  }
+];
