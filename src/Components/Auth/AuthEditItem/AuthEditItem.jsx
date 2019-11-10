0597052693685be9d75ceb2b52855c438ff4185a
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-11-09 17:53:26 -1000

    adding more to detail item page

--- src/Components/Auth/AuthEditItem/AuthEditItem.jsx
+++ src/Components/Auth/AuthEditItem/AuthEditItem.jsx
@@ -1,10 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-class AuthEditItem extends Component {
-  state = {};
-  render() {
-    return <div>Hello Meow</div>;
-  }
-}
+const AuthEditItem = ({ ...props }) => {
+  return <div>Hello Meow</div>;
+};
 
 export default AuthEditItem;
