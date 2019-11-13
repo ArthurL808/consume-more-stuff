6902fc4faaf48e996466b896ac4ad11df5709dc5
Arthur <kingart808@gmail.com>
2019-11-13 00:16:06 -1000

    got edit working but still trying to get fields to generate original data and have original image show as well

--- server/routes/api/items/items.js
+++ server/routes/api/items/items.js
@@ -1,83 +1,88 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const upload = require('../../../services/file-upload')
-const singleUpload = upload.single('image')
+const upload = require("../../../services/file-upload");
+const singleUpload = upload.single("image");
 
 //localhost:8080/api/items
-router.get('/', (req, res) => {
-    return req.db.Items.fetchAll({withRelated: ['user','category','itemStatus','condition']})
-        .then((results) => {
-            res.send(results.toJSON());
-
-        });
+router.get("/", (req, res) => {
+  return req.db.Items.fetchAll({
+    withRelated: ["user", "category", "itemStatus", "condition"]
+  }).then(results => {
+    res.send(results.toJSON());
+  });
 });
-router.get('/:id', (req, res) => {
-    return req.db.Items.where({ id: req.params.id })
-        .fetch({withRelated: ['user','category','itemStatus','condition']})
-        .then((item) => {
-            res.json(item);
-        })
-        .catch((error) => {
-            console.log('Error: ', error);
-            res.status(400).send({ message: 'Error fetching item.' })
-        })
-})
-
-
-router.post('/new',singleUpload, (req, res) => {
-    let newItem = {
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        manufacturer: req.body.make,
-        imageUrl: req.file.location,
-        user_id: 1,
-        category_id: req.body.category,
-        itemStatus_id: req.body.itemStatus,
-        condition_id: req.body.condition
-    };
-    return req.db.Items.forge(newItem)
-        .save()
-        .then(() => {
-            res.status(200).json({ message: 'Item posted!' })
-        })
-        .catch((error) => {
-            console.log('Error: ', error);
-            res.status(500).json({ message: 'Could not post item.' })
-        });
+router.get("/:id", (req, res) => {
+  return req.db.Items.where({ id: req.params.id })
+    .fetch({ withRelated: ["user", "category", "itemStatus", "condition"] })
+    .then(item => {
+      res.json(item);
+    })
+    .catch(error => {
+      console.log("Error: ", error);
+      res.status(400).send({ message: "Error fetching item." });
+    });
 });
 
-router.put('/edit', (req, res) => {
-    let editItem = {
-        id: req.body.id,
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price,
-        manufacturer: req.body.manufacturer
-    };
-    return req.db.Items.forge({ user_id: req.body.user_id })
-        .save(editItem, { patch: true })
-        .then(() => {
-            res.status(200).redirect('/api/items');
-        })
-        .catch((error) => {
-            console.log("Error: ", error);
-            res.status(500).json({ messgae: 'Could not edit item.' })
-        });
+router.post("/new", singleUpload, (req, res) => {
+  let newItem = {
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    manufacturer: req.body.make,
+    imageUrl: req.file.location,
+    user_id: 1,
+    category_id: req.body.category,
+    itemStatus_id: req.body.itemStatus,
+    condition_id: req.body.condition
+  };
+  return req.db.Items.forge(newItem)
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "Item posted!" });
+    })
+    .catch(error => {
+      console.log("Error: ", error);
+      res.status(500).json({ message: "Could not post item." });
+    });
 });
 
-router.delete('/delete', (req, res) => {
-    let itemID = req.body.id;
-    let userID = req.body.user_id;
-    return req.db.Items.where({ id: itemID, user_id: userID })
-        .destroy()
-        .then(() => {
-            res.status(200).redirect('/api/items');
-        })
-        .catch((error) => {
-            console.log('Error: ', error);
-            res.status(500).send({ message: 'Item could not be deleted.' })
-        });
+router.put("/:id", singleUpload, (req, res) => {
+  let newItem = {
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    manufacturer: req.body.make,
+    user_id: 1,
+    category_id: req.body.category,
+    itemStatus_id: req.body.itemStatus,
+    condition_id: req.body.condition
+  };
+  if (!req.file) {
+      newItem.imageUrl = req.body.imageUrl;
+} else if(!req.body.imageUrl){
+    newItem.imageUrl = req.file.location;
+  }else{
+      newItem.imageUrl = 'https://consume-more-stuff-images.s3-us-west-2.amazonaws.com/1572740907680'
+  }
+  return req.db.Items.forge({ id: req.params.id })
+    .save(newItem)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  return req.db.Items.where({ id: req.params.id })
+    .destroy()
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 });
 
 module.exports = router;
