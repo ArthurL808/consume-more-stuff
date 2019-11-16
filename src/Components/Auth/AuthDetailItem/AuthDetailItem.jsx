452507fc0cf1b205cf6b4d7d68f074ca3d92a234
frzrbox <david@freezerbox.dev>
2019-11-16 12:32:16 -1000

    added back rina styles

--- src/Components/Auth/AuthDetailItem/AuthDetailItem.jsx
+++ src/Components/Auth/AuthDetailItem/AuthDetailItem.jsx
@@ -15,53 +15,54 @@ const AuthDetailItem = ({ ...props }) => {
   }, [dispatch, id]);
 
   return (
-    <div>
-      <button className={styles.back_btn}>
+    <Fragment>
+      <button>
         <Link to="/">Go Back</Link>
       </button>
-      <div key={id} id={styles.detail_container} className="container">
+
+      <div key={id} className={styles.detailBody}>
         {item && (
           <>
-            <div className={styles.img_container}>
+            <div className={styles.imageAndName}>
+              <h2>{item.name}</h2>
               <img src={item.imageUrl} alt={item.name} />
+              <button>
+                <Link
+                  to={location => ({
+                    ...location,
+                    pathname: `/edit/${item.id}`
+                  })}
+                >
+                  Edit Item
+                </Link>
+              </button>
+              {/* <button onClick={this.handleDelete}> Delete Item </button> */}
             </div>
-            <h3>{item.name}</h3>
 
-            <h4>Description:</h4>
-            <p>{item.description}</p>
-            <h4>Price:</h4>
-            <p>{item.price}</p>
-            <h4>Manufacturer:</h4>
-            <p>{item.manufacturer}</p>
-            <h4>Condition</h4>
-            <p>{item.condition.condition}</p>
-            <h4>Category</h4>
-            <p>{item.category.category}</p>
-            <h5>Created At</h5>
-            <p>{createdDate}</p>
-            <h5>Updated At</h5>
-            <p>{updatedDate}</p>
-
-            <button>
-              <Link
-                to={location => ({
-                  ...location,
-                  pathname: `/edit/${item.id}`
-                })}
-              >
-                Edit Item
-              </Link>
-            </button>
+            <div className={styles.detailText}>
+              <h4>Description:</h4>
+              <p>{item.description}</p>
+              <h4>Price:</h4>
+              <p>{item.price}</p>
+              <h4>Manufacturer:</h4>
+              <p>{item.manufacturer}</p>
+              <h4>Condition:</h4>
+              <p>{item.condition.condition}</p>
+              <h4>Category:</h4>
+              <p>{item.category.category}</p>
+              <h4>Created At:</h4>
+              <p>{createdDate}</p>
+              <h4>Updated At:</h4>
+              <p>{updatedDate}</p>
+            </div>
           </>
         )}
       </div>
-    </div>
+    </Fragment>
   );
 };
-
 const mapStateToProps = state => {
   console.log(state);
   return { item: state.item };
 };
-
 export default connect(mapStateToProps)(AuthDetailItem);
