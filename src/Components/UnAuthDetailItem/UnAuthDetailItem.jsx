7e5ae6edbefcd1f75cc28fbb87468d70095a183f
frzrbox <david@freezerbox.dev>
2019-11-16 12:40:41 -1000

    updates styls of unauth to match auth

--- src/Components/UnAuthDetailItem/UnAuthDetailItem.jsx
+++ src/Components/UnAuthDetailItem/UnAuthDetailItem.jsx
@@ -2,9 +2,9 @@ import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import { loadItemAsync } from "../../actions";
 import { Link } from "react-router-dom";
-import styles from './UnAuthDetailItem.module.scss'
+import styles from "./UnAuthDetailItem.module.scss";
 
-const UnAuthDetailItem = ({ ...props }) => {
+const AuthDetailItem = ({ ...props }) => {
     const { dispatch, item, match } = props;
     const id = parseInt(match.params.id);
     const createdDate = item && new Date(item.created_at).toUTCString();
@@ -16,45 +16,42 @@ const UnAuthDetailItem = ({ ...props }) => {
 
     return (
         <Fragment>
-            <button className={styles.back_btn}>
-                <Link to="/all">Go Back</Link>
+            <button>
+                <Link to="/">Go Back</Link>
             </button>
 
-            <div key={id} id={styles.detail_container} >
+            <div key={id} className={styles.detailBody}>
                 {item && (
                     <>
-                        <div className={styles.img_container}>
+                        <div className={styles.imageAndName}>
+                            <h2>{item.name}</h2>
                             <img src={item.imageUrl} alt={item.name} />
                         </div>
 
-                        <div className="container">
-                            <h3>{item.name}</h3>
-
+                        <div className={styles.detailText}>
                             <h4>Description:</h4>
                             <p>{item.description}</p>
                             <h4>Price:</h4>
                             <p>{item.price}</p>
                             <h4>Manufacturer:</h4>
                             <p>{item.manufacturer}</p>
-                            <h4>Condition</h4>
+                            <h4>Condition:</h4>
                             <p>{item.condition.condition}</p>
-                            <h4>Category</h4>
+                            <h4>Category:</h4>
                             <p>{item.category.category}</p>
-                            <h5>Created At</h5>
+                            <h4>Created At:</h4>
                             <p>{createdDate}</p>
-                            <h5>Updated At</h5>
+                            <h4>Updated At:</h4>
                             <p>{updatedDate}</p>
                         </div>
-
                     </>
                 )}
             </div>
         </Fragment>
     );
 };
-
 const mapStateToProps = state => {
+    console.log(state);
     return { item: state.item };
 };
-
-export default connect(mapStateToProps)(UnAuthDetailItem);
+export default connect(mapStateToProps)(AuthDetailItem);
